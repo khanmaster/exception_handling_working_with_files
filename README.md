360bdc70c7ecbdbe3fe0c8e64f34e5bc24e29a1e
khanmaster <eashkhan@hotmail.com>
2021-06-30 14:14:50 +0100

    added DRY with file and exception handling code examples

--- README.md
+++ README.md
@@ -24,3 +24,54 @@
 - `try` works as `if condition`
 - `except` works as `elif`
 - `finally` works as else, finally will execute regardless of `try` and `except` conditions  
+
+# Let's see these in action
+```python
+file = open("order.text") # Looks for the file called order.text
+print(file)
+#run program
+
+# first iteration - using exception handling
+try:
+    file = open("order.text")
+    print("file is found")
+except:
+    print("file not found")
+
+# second iteration with capturing error message
+try:
+    file = open("order.text")
+    print("file found ")
+except FileNotFoundError as errmsg: # add alias
+    print(f"The above block of code was not executed as {errmsg} found ")
+finally:
+    print("Thank you visiting")
+
+# Apply DRY
+def open_using_with_and_print(file):
+    try:
+        with open(file, "r") as file:
+            for line in file.readlines():
+                print(line.strip('\n'))
+    except FileNotFoundError:
+        print("file can't be found please check the name")
+        #raise
+    finally:
+        print("\nPlease choose the items from the list and enjoy your HAPPY MEAL")
+
+open_using_with_and_print("order.text")
+```
+
+```python
+
+def write_to_file(file, order_item):
+    try:
+        with open(file, "w") as file: # the `w` has the functionality abstracted
+            file.write(order_item + '\n')
+    except FileNotFoundError:
+        print("file cannot be found or directory is incorrect, please check the details provided")
+        raise
+
+write_to_file("food.text", "apple")
+
+```
--- files_and_exeception_handling.py
+++ files_and_exeception_handling.py
@@ -1,3 +1,70 @@
 
 
-print(1/0)
+# print(1/0) #ZeroDivisionError: division by zero
+#
+# num = 9
+# if num > 8: # this would throw syntax error SyntaxError: invalid syntax
+#     print(num)
+
+# we will create a file with required permission and see what errors/exception are possible to be seen
+# first iteration
+# file = open("order.text") # open() takes a string arg with file name
+# print(file) # Let's see the outcome and record it
+
+# # second iteration
+# try:
+#     file = open("order.text")
+#     print("file found") # try block required except or will throw an error
+#
+# except FileNotFoundError as errmsg: # creating alias same as a nick name
+#     print(f"file not found {errmsg}")
+#
+# finally: # Finally will execute regardless of try and except block execution, also used to clean up the code
+#     print("Thank you for visiting See you again!")
+
+# Let's create a file called order.text - naming is essential so ensure it has the same name as above
+
+# Let's apply DRY - OOP - Python Packaging
+           #   1     2         3
+#
+# # 3rd Iteration
+# # Create a function to the same job DRY
+
+def open_using_with_and_print(file): # takes one string arg
+    try:
+        with open(file, "r") as file: # file with read permission
+            for line in file.readlines(): # iterating through data from the file with readlines()
+                print(line.rstrip('\n')) # strinpping and printing data in each line
+
+    except FileNotFoundError:
+        print("file cannot be found or directory is incorrect, please check the details provided")
+        raise
+
+    finally:
+        print("\nPlease chose the items from the list  and enjoy your HAPPY MEAL")
+
+open_using_with_and_print("order.text") # calling the function
+
+
+
+
+
+
+
+
+
+
+
+# fourth iteration
+# add desert and Ice cream to orders.text
+
+# def write_to_file(file, order_item): # function to add items into the file
+#     try:
+#         with open(file, "w") as file: # file with write permission
+#             file.write(order_item + '\n') # adding an item
+#     except FileNotFoundError:
+#         print("file cannot be found or directory is incorrect, please check the details provided")
+#         raise
+#
+# write_to_file("order.text", "Ice Cream") # calling function and pass item name to be added to the order.text
+
